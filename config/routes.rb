ecf52f2975ff3a75fae1fd62807e71185ad4dec8
nluuarch@gmail.com <nluuarch@gmail.com>
2022-02-02 22:46:37 -0800

    Added controllers for user auth

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,12 @@ Rails.application.routes.draw do
   resources :user_conversations, only:[:create, :destroy] 
   resources :listings #all routes
   resources :users #all routes
+
+
+  # for user authentication
+  post "/signup", to: "users#create"
+  get "/me", to: "users#show"
+  post "/login", to: "sessions#create"
+  delete "/logout", to: "sessions#destroy"
  
 end
